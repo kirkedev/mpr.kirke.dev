d0647b6afe6a2019b046c158cdda85759c01a303
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2021-10-12 20:44:11 -0500

    Add tests for parsing and serializing current day purchases

--- test/lib/purchases.spec.ts
+++ test/lib/purchases.spec.ts
@@ -9,7 +9,7 @@ import load from "./resources";
 const deserialize = (purchase: PurchaseResponse): Purchase =>
     Purchase.parse(purchase)[0];
 
-describe("Parse daily purchase records", () => {
+describe("Parse prior day purchase records", () => {
     const records = Array.from(parse(load<BarrowsGilts>("prior_day_purchases.json")));
     const serialized = serialize(records);
 
@@ -163,6 +163,165 @@ describe("Parse daily purchase records", () => {
             const record = serialized[6];
             expect(record.date).toBe("2020-04-07");
             expect(record.reportDate).toBe("2020-04-08");
+            expect(deserialize([record])).toEqual(records[6]);
+        });
+    });
+});
+
+describe("Parse afternoon purchase records", () => {
+    const records = Array.from(parse(load<BarrowsGilts>("purchases.json")));
+    const serialized = serialize(records);
+
+    describe("Negotiated, carcass basis", () => {
+        test("parse", () => {
+            const record = records[0];
+            expect(record.date).toEqual(new Date(2020, 3, 9));
+            expect(record.reportDate).toEqual(new Date(2020, 3, 9));
+            expect(record.seller).toBe(Seller.All);
+            expect(record.arrangement).toBe(Arrangement.Negotiated);
+            expect(record.basis).toBe(Basis.Carcass);
+            expect(record.headCount).toBe(5481);
+            expect(record.avgPrice).toBeCloseTo(39.96);
+            expect(record.lowPrice).toBeCloseTo(38.00);
+            expect(record.highPrice).toBeCloseTo(41.00);
+        });
+
+        test("serialize", () => {
+            const record = serialized[0];
+            expect(record.date).toBe("2020-04-09");
+            expect(record.reportDate).toBe("2020-04-09");
+            expect(deserialize([record])).toEqual(records[0]);
+        });
+    });
+
+    describe("Negotiated formula, carcass basis", () => {
+        test("parse", () => {
+            const record = records[1];
+            expect(record.date).toEqual(new Date(2020, 3, 9));
+            expect(record.reportDate).toEqual(new Date(2020, 3, 9));
+            expect(record.seller).toBe(Seller.All);
+            expect(record.arrangement).toBe(Arrangement.NegotiatedFormula);
+            expect(record.basis).toBe(Basis.Carcass);
+            expect(record.headCount).toBe(1022);
+            expect(record.avgPrice).toBeCloseTo(49.28);
+            expect(record.lowPrice).toBeCloseTo(40.34);
+            expect(record.highPrice).toBeCloseTo(56.02);
+        });
+
+        test("serialize", () => {
+            const record = serialized[1];
+            expect(record.date).toBe("2020-04-09");
+            expect(record.reportDate).toBe("2020-04-09");
+            expect(deserialize([record])).toEqual(records[1]);
+        });
+    });
+
+    describe("All negotiated, carcass basis", () => {
+        test("parse", () => {
+            const record = records[2];
+            expect(record.date).toEqual(new Date(2020, 3, 9));
+            expect(record.reportDate).toEqual(new Date(2020, 3, 9));
+            expect(record.seller).toBe(Seller.All);
+            expect(record.arrangement).toBe(Arrangement.AllNegotiated);
+            expect(record.basis).toBe(Basis.Carcass);
+            expect(record.headCount).toBe(6503);
+            expect(record.avgPrice).toBeCloseTo(41.43);
+            expect(record.lowPrice).toBeCloseTo(38.00);
+            expect(record.highPrice).toBeCloseTo(56.02);
+        });
+
+        test("serialize", () => {
+            const record = serialized[2];
+            expect(record.date).toBe("2020-04-09");
+            expect(record.reportDate).toBe("2020-04-09");
+            expect(deserialize([record])).toEqual(records[2]);
+        });
+    });
+
+    describe("Market formula, carcass basis", () => {
+        test("parse", () => {
+            const record = records[3];
+            expect(record.date).toEqual(new Date(2020, 3, 9));
+            expect(record.reportDate).toEqual(new Date(2020, 3, 9));
+            expect(record.seller).toBe(Seller.All);
+            expect(record.arrangement).toBe(Arrangement.MarketFormula);
+            expect(record.basis).toBe(Basis.Carcass);
+            expect(record.headCount).toBe(130_169);
+            expect(record.avgPrice).toBeCloseTo(46.14);
+            expect(record.lowPrice).toBeCloseTo(38.50);
+            expect(record.highPrice).toBeCloseTo(60.40);
+        });
+
+        test("serialize", () => {
+            const record = serialized[3];
+            expect(record.date).toBe("2020-04-09");
+            expect(record.reportDate).toBe("2020-04-09");
+            expect(deserialize([record])).toEqual(records[3]);
+        });
+    });
+
+    describe("Negotiated, live basis", () => {
+        test("parse", () => {
+            const record = records[4];
+            expect(record.date).toEqual(new Date(2020, 3, 9));
+            expect(record.reportDate).toEqual(new Date(2020, 3, 9));
+            expect(record.seller).toBe(Seller.All);
+            expect(record.arrangement).toBe(Arrangement.Negotiated);
+            expect(record.basis).toBe(Basis.Live);
+            expect(record.headCount).toBe(990);
+            expect(record.avgPrice).toBeCloseTo(29.35);
+            expect(record.lowPrice).toBeCloseTo(19.00);
+            expect(record.highPrice).toBeCloseTo(33.30);
+        });
+
+        test("serialize", () => {
+            const record = serialized[4];
+            expect(record.date).toBe("2020-04-09");
+            expect(record.reportDate).toBe("2020-04-09");
+            expect(deserialize([record])).toEqual(records[4]);
+        });
+    });
+
+    describe("Negotiated formula, live basis", () => {
+        test("parse", () => {
+            const record = records[5];
+            expect(record.date).toEqual(new Date(2020, 3, 9));
+            expect(record.reportDate).toEqual(new Date(2020, 3, 9));
+            expect(record.seller).toBe(Seller.All);
+            expect(record.arrangement).toBe(Arrangement.NegotiatedFormula);
+            expect(record.basis).toBe(Basis.Live);
+            expect(record.headCount).toBe(165);
+            expect(record.avgPrice).toBeNaN();
+            expect(record.lowPrice).toBeNaN();
+            expect(record.highPrice).toBeNaN();
+        });
+
+        test("serialize", () => {
+            const record = serialized[5];
+            expect(record.date).toBe("2020-04-09");
+            expect(record.reportDate).toBe("2020-04-09");
+            expect(deserialize([record])).toEqual(records[5]);
+        });
+    });
+
+    describe("All negotiated, live basis", () => {
+        test("parse", () => {
+            const record = records[6];
+            expect(record.date).toEqual(new Date(2020, 3, 9));
+            expect(record.reportDate).toEqual(new Date(2020, 3, 9));
+            expect(record.seller).toBe(Seller.All);
+            expect(record.arrangement).toBe(Arrangement.AllNegotiated);
+            expect(record.basis).toBe(Basis.Live);
+            expect(record.headCount).toBe(1155);
+            expect(record.avgPrice).toBeNaN();
+            expect(record.lowPrice).toBeNaN();
+            expect(record.highPrice).toBeNaN();
+        });
+
+        test("serialize", () => {
+            const record = serialized[6];
+            expect(record.date).toBe("2020-04-09");
+            expect(record.reportDate).toBe("2020-04-09");
             expect(deserialize([record])).toEqual(records[6]);
         });
     });
