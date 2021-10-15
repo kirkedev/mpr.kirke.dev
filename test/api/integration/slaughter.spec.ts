91beed4122d1a50ea37c29fb0f4064ccfed947f1
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2021-10-15 15:08:07 -0500

    Add assertions to slaughter api integration test

--- test/api/integration/slaughter.spec.ts
+++ test/api/integration/slaughter.spec.ts
@@ -1,13 +1,13 @@
 import Slaughter from "lib/slaughter";
+import cashIndex from "../../../lib/CashIndex";
 
-it("should return slaughter records by date range", () => {
-    cy.request("/slaughter?start=2021-08-09&end=2021-08-13")
-        .should(response => {
-            expect(response.status).to.equal(200);
+describe("slaughter records", () => {
+    it("should return slaughter records by date range", () => {
+        cy.request("/slaughter?start=2021-08-09&end=2021-08-13").should(({ status, body }) => {
+            expect(status).to.equal(200);
+            expect(body.length).to.equal(40);
 
-            const slaughter = Slaughter.parse(response.body);
-            expect(slaughter.length).to.equal(40);
-
+            const slaughter = Slaughter.parse(body);
             const dates = Array.from(new Set(slaughter.map(({ date }) => date.getTime())))
                 .map(timestamp => new Date(timestamp));
 
@@ -19,4 +19,19 @@ it("should return slaughter records by date range", () => {
                 new Date(2021, 7, 13)
             ]);
         });
+    });
+
+    it("should be able to calculate the CME Lean Hog Index", () => {
+        cy.request("/slaughter?start=2021-08-06&end=2021-08-13").should(({ body }) => {
+            const cash = Array.from(cashIndex(Slaughter.parse(body)));
+
+            expect(cash).to.deep.equal([
+                { date: new Date(2021, 7, 9), dailyPrice: 110.59, indexPrice: 110.77 },
+                { date: new Date(2021, 7, 10), dailyPrice: 110.32, indexPrice: 110.45 },
+                { date: new Date(2021, 7, 11), dailyPrice: 110.06, indexPrice: 110.19 },
+                { date: new Date(2021, 7, 12), dailyPrice: 109.75, indexPrice: 109.90 },
+                { date: new Date(2021, 7, 13), dailyPrice: 109.59, indexPrice: 109.67 }
+            ]);
+        });
+    });
 });
