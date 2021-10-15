39cd5b9c6d55ba3242367a235c4f82525443ee42
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2021-10-15 14:40:57 -0500

    Fix test data to index by record date instead of report date

--- test/api/integration/slaughter.spec.ts
+++ test/api/integration/slaughter.spec.ts
@@ -1,11 +1,22 @@
 import Slaughter from "lib/slaughter";
 
-it("should return slaughter records by a date range", () => {
-    cy.request("/slaughter?start=2021-09-06&end=2021-09-10")
+it("should return slaughter records by date range", () => {
+    cy.request("/slaughter?start=2021-08-09&end=2021-08-13")
         .should(response => {
-            expect(response.status).to.eq(200);
+            expect(response.status).to.equal(200);
 
             const slaughter = Slaughter.parse(response.body);
-            expect(slaughter.length).to.eq(24);
+            expect(slaughter.length).to.equal(40);
+
+            const dates = Array.from(new Set(slaughter.map(({ date }) => date.getTime())))
+                .map(timestamp => new Date(timestamp));
+
+            expect(dates).to.deep.equal([
+                new Date(2021, 7, 9),
+                new Date(2021, 7, 10),
+                new Date(2021, 7, 11),
+                new Date(2021, 7, 12),
+                new Date(2021, 7, 13)
+            ]);
         });
 });
