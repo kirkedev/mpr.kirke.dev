061d9b2cc300a14112caf1dcf4a2e42dd4bba3a8
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2021-10-25 17:50:39 -0500

    Refactor tailwind plugin

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,12 +1,13 @@
-const extractColorVars = (colorObj, colorGroup = "") =>
+const svgUtilities = (colorObj, colorGroup = "") =>
     Object.keys(colorObj).reduce((vars, colorKey) => {
         const value = colorObj[colorKey];
+
         const newVars = typeof value === "string"
             ? {
                 [`.stroke${colorGroup}-${colorKey}`]: { stroke: value },
                 [`.fill${colorGroup}-${colorKey}`]: { fill: value }
             }
-            : extractColorVars(value, `-${colorKey}`);
+            : svgUtilities(value, `-${colorKey}`);
 
         return { ...vars, ...newVars };
     }, {});
@@ -15,21 +16,10 @@ module.exports = {
     mode: "jit",
     purge: ["index.html", "**/*.tsx", "**/*.module.css"],
     darkMode: false,
-    theme: {
-        extend: {
-            fill: {
-                none: "none"
-            }
-        },
-    },
     variants: {
         extend: {},
     },
     plugins: [
-        function({ addUtilities, theme }) {
-            const colors = extractColorVars(theme("colors"));
-            console.log(colors)
-            addUtilities(colors);
-        }
+        ({ addUtilities, theme }) => addUtilities(svgUtilities(theme("colors")))
     ]
 }
