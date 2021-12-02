898dee2a46562eb77ab6d3cf60581675cd150fdb
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2021-12-01 22:36:57 -0600

    Add purchases api route

--- api/api.ts
+++ api/api.ts
@@ -2,10 +2,12 @@ import fastify from "fastify";
 import getHealth from "./health";
 import getCutout, { CutoutSchema } from "./cutout";
 import getSlaughter, { SlaughterSchema } from "./slaughter";
+import getPurchases, { PurchaseSchema } from "./purchases";
 
 const api = fastify({ logger: process.env.NODE_ENV !== "test" })
     .get("/health", getHealth)
     .get("/cutout", { schema: CutoutSchema }, getCutout)
-    .get("/slaughter", { schema: SlaughterSchema }, getSlaughter);
+    .get("/slaughter", { schema: SlaughterSchema }, getSlaughter)
+    .get("/purchases", { schema: PurchaseSchema }, getPurchases);
 
 export default api;
---
+++ api/purchases.ts
@@ -0,0 +1,37 @@
+import type { FastifyRequest, FastifySchema } from "fastify";
+import { formatDate, getDate } from "lib";
+import type { QueryType } from "lib/DateRangeQuery";
+import DateRangeQuery from "lib/DateRangeQuery";
+import Repository from "lib/Repository";
+import type Purchase from "lib/purchases";
+import type { PurchaseRecord } from "lib/purchases/mpr";
+import parse from "lib/purchases/parse";
+import PurchaseResponse from "lib/purchases/response";
+import client from "./client";
+
+interface SlaughterQuery {
+    Querystring: QueryType;
+}
+
+const PurchaseSchema: FastifySchema = {
+    querystring: DateRangeQuery,
+    response: {
+        200: PurchaseResponse
+    }
+};
+
+const report = client.report(3458).section<PurchaseRecord>("National Volume and Price Data");
+
+const fetch = (start: Date, end: Date): Promise<Purchase[]> =>
+    report.between("reported_for_date", formatDate(start), formatDate(end))
+        .get()
+        .then(response => Array.from(parse(response)));
+
+const repository = new Repository(fetch);
+
+const getPurchases = ({ query }: FastifyRequest<SlaughterQuery>): Promise<Purchase[]> =>
+    repository.query(getDate(query.start), getDate(query.end));
+
+export default getPurchases;
+
+export { PurchaseSchema };
