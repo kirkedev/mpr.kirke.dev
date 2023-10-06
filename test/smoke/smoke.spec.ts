b5fcdf44a91aae17b200a5ea2c2c767eaa1c8c22
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-06 12:20:19 -0400

    Condense smoke tests to one test and add assertions for stats

--- test/smoke/smoke.spec.ts
+++ test/smoke/smoke.spec.ts
@@ -1,28 +1,24 @@
-describe("The app loads", function() {
-    beforeEach(() => cy.visit("/"));
+it("loads", function() {
+    cy.visit("/");
+    cy.title().should("eq", "Mpr Dashboard");
+    cy.get("#app").should("exist");
 
-    it("should show the title", () => {
-        cy.title().should("eq", "Mpr Dashboard");
-    });
+    // Stats
+    cy.get("div[class*=cash]").find("h2[class*=value]").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=cutout]").find("h5[class*=label]").contains("Cutout").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=cutout]").find("h5[class*=label]").contains("Index").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=purchases]").find("h5[class*=label]").contains("Formula").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Belly").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Ham").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Loin").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Butt").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Rib").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Picnic").find("+h3").contains(/\d{2,3}\.\d{2}/);
 
-    it("should display the app", () => {
-        cy.get("#app").should("exist");
-    });
-
-    it("should display the cash index chart", () => {
-        cy.get("div[class*=cash] path.series").invoke("attr", "d").its("length").should("be.greaterThan", 100);
-    });
-
-    it("should display the cutout chart", () => {
-        cy.get("div[class*=cutout] path.series").first().invoke("attr", "d").its("length").should("be.greaterThan", 100);
-        cy.get("div[class*=cutout] path.series").last().invoke("attr", "d").its("length").should("be.greaterThan", 100);
-    });
-
-    it("should display the purchases chart", () => {
-        cy.get("div[class*=purchases] path.series").invoke("attr", "d").its("length").should("be.greaterThan", 100);
-    });
-
-    it("should display the primals chart", () => {
-        cy.get("div[class*=primal] path.series").invoke("attr", "d").its("length").should("be.greaterThan", 100);
-    });
+    // Charts
+    cy.get("div[class*=cash] path.series").invoke("attr", "d").its("length").should("be.greaterThan", 100);
+    cy.get("div[class*=cutout] path.series").first().invoke("attr", "d").its("length").should("be.greaterThan", 100);
+    cy.get("div[class*=cutout] path.series").last().invoke("attr", "d").its("length").should("be.greaterThan", 100);
+    cy.get("div[class*=purchases] path.series").invoke("attr", "d").its("length").should("be.greaterThan", 100);
+    cy.get("div[class*=primal] path.series").invoke("attr", "d").its("length").should("be.greaterThan", 100);
 });
