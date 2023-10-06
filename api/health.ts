0ba9d8242834fdd4ada668bf40a17323800a9a92
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-05 22:53:50 -0400

    Reformat health status handler

--- api/health.ts
+++ api/health.ts
@@ -1,16 +1,12 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
-
 interface HealthStatus {
     uptime: number;
     timestamp: number;
 }
 
-function getHealth(_: FastifyRequest, reply: FastifyReply): void {
-    reply.send({
-        uptime: process.uptime(),
-        timestamp: Date.now()
-    });
-}
+const getHealth = (): HealthStatus => ({
+    uptime: process.uptime(),
+    timestamp: Date.now()
+});
 
 export default getHealth;
 export type { HealthStatus };
