c6aa25d736621894e927fc1a44a677e9fb32acdb
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-05 20:32:07 -0400

    Remove healthcheck

--- api/healthcheck.js
+++
@@ -1,9 +0,0 @@
-import http from "http";
-
-http.get("http://0.0.0.0/health", response => {
-    console.log(`HEALTH CHECK STATUS: ${response.statusCode} ${response.statusMessage}`);
-    process.exit(response.statusCode === 200 ? 0 : 1);
-}).on("error", error => {
-    console.error(`HEALTH CHECK ERROR: ${error.message}`);
-    process.exit(1);
-}).end();
