bc2c7bc81e82785217ed49b8b40074be9c7caa20
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-06 12:35:17 -0400

    Use cypress testing library for cleaner selectors

--- test/smoke/smoke.spec.ts
+++ test/smoke/smoke.spec.ts
@@ -1,19 +1,24 @@
+import "@testing-library/cypress/add-commands";
+
 it("loads", function() {
+    const value = /\d{2,3}\.\d{2}/;
+
+    // Page load
     cy.visit("/");
     cy.title().should("eq", "Mpr Dashboard");
     cy.get("#app").should("exist");
 
     // Stats
-    cy.get("div[class*=cash]").find("h2[class*=value]").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=cutout]").find("h5[class*=label]").contains("Cutout").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=cutout]").find("h5[class*=label]").contains("Index").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=purchases]").find("h5[class*=label]").contains("Formula").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Belly").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Ham").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Loin").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Butt").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Rib").find("+h3").contains(/\d{2,3}\.\d{2}/);
-    cy.get("div[class*=primal]").find("h5[class*=label]").contains("Picnic").find("+h3").contains(/\d{2,3}\.\d{2}/);
+    cy.findByText("Cash Index").siblings().contains(value).should("exist");
+    cy.findByText("Cutout").siblings().contains(value).should("exist");
+    cy.findByText("Index").siblings().contains(value).should("exist");
+    cy.findByText("Formula").siblings().contains(value).should("exist");
+    cy.findByText("Belly").siblings().contains(value).should("exist");
+    cy.findByText("Ham").siblings().contains(value).should("exist");
+    cy.findByText("Loin").siblings().contains(value).should("exist");
+    cy.findByText("Butt").siblings().contains(value).should("exist");
+    cy.findByText("Rib").siblings().contains(value).should("exist");
+    cy.findByText("Picnic").siblings().contains(value).should("exist");
 
     // Charts
     cy.get("div[class*=cash] path.series").invoke("attr", "d").its("length").should("be.greaterThan", 100);
