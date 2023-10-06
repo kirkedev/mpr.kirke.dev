ecc3d4702721bd93861a97d4b100ac4900f8ef9b
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-05 20:34:17 -0400

    Explicitly specify sources for all tests

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         root: "..",
+        include: ["{api,lib}/**/*.spec.ts"],
         coverage: {
             all: true,
             provider: "istanbul",
