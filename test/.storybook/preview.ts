2f7335f4a5b41656fdc24307f792cfd167ec1cea
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-15 17:06:36 -0400

    Add global styles to storybook

--- test/.storybook/preview.ts
+++ test/.storybook/preview.ts
@@ -1,15 +1,16 @@
 import type { Preview } from "@storybook/svelte";
+import "app/index.css";
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+    parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
     },
-  },
 };
 
 export default preview;
