57abe0067ee92ab624fe2dee47f17327d68d6a9e
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-17 14:25:58 -0400

    Remove interaction tests from storybook

--- test/ui/ButtonGroup.stories.ts
+++ test/ui/ButtonGroup.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/svelte";
-import { within, userEvent } from "@storybook/testing-library";
 import ButtonGroup from "app/ui/ButtonGroup.svelte";
 import Period, { Periods } from "lib/Period";
 
@@ -13,21 +12,6 @@ export default Component;
 type Story = StoryObj<typeof Component>;
 
 export const PeriodSelector: Story = {
-    args: {
-        items: Periods,
-        selected: Period.ThreeMonths
-    },
-    play: async (context): Promise<void> => {
-        const container = within(context.canvasElement);
-        const user = userEvent.setup();
-        await user.click(container.getByText("1M"));
-        await user.click(container.getByText("3M"));
-        await user.click(container.getByText("6M"));
-        await user.click(container.getByText("1Y"));
-    }
-};
-
-export const DefaultSelection: Story = {
     args: {
         items: Periods,
         selected: Period.ThreeMonths
