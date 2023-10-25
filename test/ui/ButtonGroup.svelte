d6364d33aa4f88871854d71fbf191af2dd27116c
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-10-25 10:54:29 -0400

    Remove unneeded dependencies

---
+++ test/ui/ButtonGroup.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+    import ButtonGroup from "app/ui/ButtonGroup.svelte";
+    import { Story, Template } from "@storybook/addon-svelte-csf";
+    import Period, { Periods } from "lib/Period";
+
+    export const meta = {
+        title: "ButtonGroup",
+        component: ButtonGroup
+    };
+</script>
+
+<Template let:args>
+    <ButtonGroup {...args} on:select/>
+</Template>
+
+<Story name="Fruits" args={{
+    items: ["Apples", "Oranges", "Bananas", "Pears"]
+}}/>
+
+<Story name="Period Selector" args={{
+    items: Periods,
+    selected: Period.ThreeMonths
+}}/>
