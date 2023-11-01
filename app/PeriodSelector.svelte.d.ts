0722afecc158f533c027280311653d31653eb54f
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-01 00:59:19 -0400

    Use data selectors for active ButtonGroup item

---
+++ app/PeriodSelector.svelte.d.ts
@@ -0,0 +1,4 @@
+import ButtonGroup from "ui/ButtonGroup.svelte";
+import type Period from "lib/Period";
+
+export default class extends ButtonGroup<Period> {}
--- app/ui/ButtonGroup.d.ts
+++
@@ -1,8 +0,0 @@
-import { type SvelteComponent } from "svelte";
-
-interface ButtonGroupProps<T> {
-    items: T[];
-    selected?: T;
-}
-
-export type ButtonGroup<T> = SvelteComponent<ButtonGroupProps<T>>;
