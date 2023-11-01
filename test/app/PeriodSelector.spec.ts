ceda060f48e1e6c6a79c754466d2865e892345ee
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-01 01:10:24 -0400

    Add assertions to PeriodSelector test

--- test/app/PeriodSelector.spec.ts
+++ test/app/PeriodSelector.spec.ts
@@ -2,7 +2,14 @@ import { beforeEach, describe, expect, test, vi } from "vitest";
 import { render, within } from "@testing-library/svelte";
 import { userEvent } from "@testing-library/user-event";
 import PeriodSelector from "app/PeriodSelector.svelte";
-import Period from "lib/Period";
+import Period, { Periods } from "lib/Period";
+
+function assertSelected(this: HTMLElement, selected: string): void {
+    Periods.forEach(({ description }) => {
+        expect(within(this).getByText(description))
+            .toHaveAttribute("data-selected", `${selected === description}`);
+    });
+}
 
 describe("Period Selection", () => {
     const user = userEvent.setup();
@@ -15,36 +22,36 @@ describe("Period Selection", () => {
     });
 
     test("Clicking already selected period does nothing", async () => {
-        expect(within(container).getByText("3M")).toHaveAttribute("data-selected", "true");
+        assertSelected.call(container, "3M");
         await user.click(within(container).getByText("3M"));
-        expect(within(container).getByText("3M")).toHaveAttribute("data-selected", "true");
+        assertSelected.call(container, "3M");
         expect(handler).toHaveBeenCalledTimes(0);
     });
 
     test("Select 1M", async () => {
         await user.click(within(container).getByText("1M"));
-        expect(within(container).getByText("1M")).toHaveAttribute("data-selected", "true");
+        assertSelected.call(container, "1M");
         expect(handler).toHaveBeenCalledOnce();
         expect(handler).toHaveBeenCalledWith(expect.objectContaining({ detail: Period.OneMonth }));
     });
 
     test("Select 3M", async () => {
         await user.click(within(container).getByText("3M"));
-        expect(within(container).getByText("3M")).toHaveAttribute("data-selected", "true");
+        assertSelected.call(container, "3M");
         expect(handler).toHaveBeenCalledOnce();
         expect(handler).toHaveBeenCalledWith(expect.objectContaining({ detail: Period.ThreeMonths }));
     });
 
     test("Select 6M", async () => {
         await user.click(within(container).getByText("6M"));
-        expect(within(container).getByText("6M")).toHaveAttribute("data-selected", "true");
+        assertSelected.call(container, "6M");
         expect(handler).toHaveBeenCalledOnce();
         expect(handler).toHaveBeenCalledWith(expect.objectContaining({ detail: Period.SixMonths }));
     });
 
     test("Select 1Y", async () => {
         await user.click(within(container).getByText("1Y"));
-        expect(within(container).getByText("1Y")).toHaveAttribute("data-selected", "true");
+        assertSelected.call(container, "1Y");
         expect(handler).toHaveBeenCalledOnce();
         expect(handler).toHaveBeenCalledWith(expect.objectContaining({ detail: Period.OneYear }));
     });
