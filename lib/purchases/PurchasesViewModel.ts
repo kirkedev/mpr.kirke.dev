74c81f4b515db0331ccbb8f2f6c44ba93b8c4cb9
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-01 00:58:05 -0400

    Add Purchases ViewModel and Interactor

---
+++ lib/purchases/PurchasesViewModel.ts
@@ -0,0 +1,45 @@
+import { extent, today } from "..";
+import map from "../itertools/map";
+import type { Action } from "../Interactor";
+import type { Data } from "../Observation";
+import Observation, { type Series } from "../Observation";
+import Stat from "../Stat";
+import Purchase from ".";
+
+class PurchasesViewModel {
+    public static from = (purchases: Iterable<Purchase>): PurchasesViewModel =>
+        new PurchasesViewModel(today(), Purchase.marketFormula(purchases));
+
+    public static selectDate = (date = today()): Action<PurchasesViewModel> =>
+        model => new PurchasesViewModel(date, model.#series);
+
+    readonly #date: Date;
+    readonly #series: Series;
+
+    private constructor(date: Date, series: Series) {
+        this.#series = series;
+        this.#date = date;
+    }
+
+    public get date(): Date {
+        return this.#date;
+    }
+
+    public get stats(): Stat {
+        return Stat.from("Cash", this.#series, this.#date);
+    }
+
+    public get series(): Series {
+        return this.#series;
+    }
+
+    public get dates(): readonly [Date, Date] {
+        return Observation.extent(this.#series);
+    }
+
+    public get values(): readonly [number, number] {
+        return extent(map(this.#series, (record: Data) => record.value));
+    }
+}
+
+export default PurchasesViewModel;
