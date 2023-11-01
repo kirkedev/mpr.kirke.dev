6c4799630007af2ff4feb5dc376cd400de835c97
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-01 12:02:55 -0400

    Convert api resources to data needed by root components

--- app/api/index.ts
+++ app/api/index.ts
@@ -3,15 +3,18 @@ import Week from "lib/Week";
 import Period from "lib/Period";
 import type Cutout from "lib/cutout";
 import type Purchase from "lib/purchases";
-import type Slaughter from "lib/slaughter";
 import cutout from "./cutout";
 import slaughter from "./slaughter";
 import purchases from "./purchases";
+import CutoutIndex from "lib/cutout/CutoutIndex";
+import CashIndex from "lib/slaughter/CashIndex";
+import { dropWhile } from "lib/itertools/drop";
 
 export interface Resources {
-    cutout: Cutout[];
-    slaughter: Slaughter[];
-    purchases: Purchase[];
+    cashIndex: Iterable<CashIndex>;
+    cutoutIndex: Iterable<CutoutIndex>;
+    purchases: Iterable<Purchase>;
+    primals: Iterable<Cutout>;
 }
 
 interface ApiStore extends Readable<Promise<Resources>> {
@@ -21,7 +24,7 @@ interface ApiStore extends Readable<Promise<Resources>> {
 
 const period = writable(Period.ThreeMonths);
 
-const { subscribe } = derived(period, function(period) {
+const { subscribe } = derived(period, function(period): Promise<Resources> {
     const { start } = Week.with(period.start).previous;
     const { end } = period;
 
@@ -30,18 +33,19 @@ const { subscribe } = derived(period, function(period) {
         slaughter.query(start, end),
         purchases.query(start, end)
     ]).then(([cutout, slaughter, purchases]) => ({
-        cutout,
-        slaughter,
-        purchases
+        cashIndex: dropWhile(CashIndex.from(slaughter), record => record.date < period.start),
+        cutoutIndex: dropWhile(CutoutIndex.from(cutout), record => record.date < period.start),
+        purchases: dropWhile(purchases, record => record.date < period.start),
+        primals: dropWhile(cutout, record => record.date < period.start)
     }));
 });
 
-const store = (): ApiStore => ({
+const store: ApiStore = {
     subscribe,
     fetch: period.set,
     get period(): Period {
         return get(period);
     }
-});
+};
 
 export default store;
--- test/app/api.spec.ts
+++ test/app/api.spec.ts
@@ -2,73 +2,91 @@ import { describe, expect, test } from "vitest";
 import { get } from "svelte/store";
 import Period from "lib/Period";
 import api from "app/api";
+import { count } from "lib/itertools/accumulate";
+import { first, last } from "lib/itertools/slice";
 
 describe("fetch periods", () => {
     test("fetch one month of data", async () => {
         api.fetch(Period.OneMonth);
-        const { cutout, purchases, slaughter } = await get(api);
+        const { cutoutIndex, purchases, cashIndex, primals } = await get(api);
 
-        expect(cutout.length).toBe(28);
-        expect(cutout[0].date).toEqual(new Date(2021, 10, 15));
-        expect(cutout[27].date).toEqual(new Date(2021, 11, 23));
+        expect(count(cutoutIndex)).toBe(22);
+        expect(first(cutoutIndex).date).toEqual(new Date(2021, 10, 23));
+        expect(last(cutoutIndex).date).toEqual(new Date(2021, 11, 23));
 
-        expect(purchases.length).toBe(243);
-        expect(purchases[0].date).toEqual(new Date(2021, 10, 15));
-        expect(purchases[242].date).toEqual(new Date(2021, 11, 22));
+        expect(count(purchases)).toBe(189);
+        expect(first(purchases).date).toEqual(new Date(2021, 10, 23));
+        expect(last(purchases).date).toEqual(new Date(2021, 11, 22));
 
-        expect(slaughter.length).toBe(216);
-        expect(slaughter[0].date).toEqual(new Date(2021, 10, 15));
-        expect(slaughter[215].date).toEqual(new Date(2021, 11, 22));
+        expect(count(cashIndex)).toBe(21);
+        expect(first(cashIndex).date).toEqual(new Date(2021, 10, 23));
+        expect(last(cashIndex).date).toEqual(new Date(2021, 11, 22));
+
+        expect(count(primals)).toBe(22);
+        expect(first(primals).date).toEqual(new Date(2021, 10, 23));
+        expect(last(primals).date).toEqual(new Date(2021, 11, 23));
     });
 
     test("fetch three months of data", async () => {
         api.fetch(Period.ThreeMonths);
-        const { cutout, purchases, slaughter } = await get(api);
+        const { cutoutIndex, purchases, cashIndex, primals } = await get(api);
 
-        expect(cutout.length).toBe(73);
-        expect(cutout[0].date).toEqual(new Date(2021, 8, 13));
-        expect(cutout[72].date).toEqual(new Date(2021, 11, 23));
+        expect(count(cutoutIndex)).toBe(65);
+        expect(first(cutoutIndex).date).toEqual(new Date(2021, 8, 23));
+        expect(last(cutoutIndex).date).toEqual(new Date(2021, 11, 23));
 
-        expect(purchases.length).toBe(648);
-        expect(purchases[0].date).toEqual(new Date(2021, 8, 13));
-        expect(purchases[647].date).toEqual(new Date(2021, 11, 22));
+        expect(count(purchases)).toBe(576);
+        expect(first(purchases).date).toEqual(new Date(2021, 8, 23));
+        expect(last(purchases).date).toEqual(new Date(2021, 11, 22));
 
-        expect(slaughter.length).toBe(576);
-        expect(slaughter[0].date).toEqual(new Date(2021, 8, 13));
-        expect(slaughter[575].date).toEqual(new Date(2021, 11, 22));
+        expect(count(cashIndex)).toBe(64);
+        expect(first(cashIndex).date).toEqual(new Date(2021, 8, 23));
+        expect(last(cashIndex).date).toEqual(new Date(2021, 11, 22));
+
+        expect(count(primals)).toBe(65);
+        expect(first(primals).date).toEqual(new Date(2021, 8, 23));
+        expect(last(primals).date).toEqual(new Date(2021, 11, 23));
     });
 
     test("fetch six months of data", async () => {
         api.fetch(Period.SixMonths);
-        const { cutout, purchases, slaughter } = await get(api);
+        const { cutoutIndex, purchases, cashIndex, primals } = await get(api);
 
-        expect(cutout.length).toBe(136);
-        expect(cutout[0].date).toEqual(new Date(2021, 5, 14));
-        expect(cutout[135].date).toEqual(new Date(2021, 11, 23));
+        expect(count(cutoutIndex)).toBe(129);
+        expect(first(cutoutIndex).date).toEqual(new Date(2021, 5, 23));
+        expect(last(cutoutIndex).date).toEqual(new Date(2021, 11, 23));
 
-        expect(purchases.length).toBe(1215);
-        expect(purchases[0].date).toEqual(new Date(2021, 5, 14));
-        expect(purchases[1214].date).toEqual(new Date(2021, 11, 22));
+        expect(count(purchases)).toBe(1152);
+        expect(first(purchases).date).toEqual(new Date(2021, 5, 23));
+        expect(last(purchases).date).toEqual(new Date(2021, 11, 22));
 
-        expect(slaughter.length).toBe(1080);
-        expect(slaughter[0].date).toEqual(new Date(2021, 5, 14));
-        expect(slaughter[1079].date).toEqual(new Date(2021, 11, 22));
+        expect(count(cashIndex)).toBe(128);
+        expect(first(cashIndex).date).toEqual(new Date(2021, 5, 23));
+        expect(last(cashIndex).date).toEqual(new Date(2021, 11, 22));
+
+        expect(count(primals)).toBe(129);
+        expect(first(primals).date).toEqual(new Date(2021, 5, 23));
+        expect(last(primals).date).toEqual(new Date(2021, 11, 23));
     });
 
     test("fetch one year of data", async () => {
         api.fetch(Period.OneYear);
-        const { cutout, purchases, slaughter } = await get(api);
+        const { cutoutIndex, purchases, cashIndex, primals } = await get(api);
 
-        expect(cutout.length).toBe(262);
-        expect(cutout[0].date).toEqual(new Date(2020, 11, 14));
-        expect(cutout[261].date).toEqual(new Date(2021, 11, 23));
+        expect(count(cutoutIndex)).toBe(255);
+        expect(first(cutoutIndex).date).toEqual(new Date(2020, 11, 23));
+        expect(last(cutoutIndex).date).toEqual(new Date(2021, 11, 23));
 
-        expect(purchases.length).toBe(2349);
-        expect(purchases[0].date).toEqual(new Date(2020, 11, 14));
-        expect(purchases[2348].date).toEqual(new Date(2021, 11, 22));
+        expect(count(purchases)).toBe(2286);
+        expect(first(purchases).date).toEqual(new Date(2020, 11, 23));
+        expect(last(purchases).date).toEqual(new Date(2021, 11, 22));
 
-        expect(slaughter.length).toBe(2088);
-        expect(slaughter[0].date).toEqual(new Date(2020, 11, 14));
-        expect(slaughter[2087].date).toEqual(new Date(2021, 11, 22));
+        expect(count(cashIndex)).toBe(254);
+        expect(first(cashIndex).date).toEqual(new Date(2020, 11, 23));
+        expect(last(cashIndex).date).toEqual(new Date(2021, 11, 22));
+
+        expect(count(primals)).toBe(255);
+        expect(first(primals).date).toEqual(new Date(2020, 11, 23));
+        expect(last(primals).date).toEqual(new Date(2021, 11, 23));
     });
 });
