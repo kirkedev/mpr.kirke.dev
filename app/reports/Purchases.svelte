cb55d89a43dcaa918f4b72b08869679c749de7a5
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-16 18:39:03 -0500

    Add Purchases Report

---
+++ app/reports/Purchases.svelte
@@ -0,0 +1,41 @@
+<script lang="ts" context="module">
+    import type Purchase from "lib/purchases";
+    import PurchasesInteractor from "lib/purchases/PurchasesInteractor";
+    import ObservationChart from "../ui/ObservationChart.svelte";
+    import Circle from "../ui/Circle.svelte";
+    import Marker from "../ui/Marker.svelte";
+    import Path from "../ui/Path.svelte";
+    import store from "../store";
+</script>
+
+<style lang="postcss">
+    @import "Purchases.css";
+</style>
+
+<script lang="ts">
+    export let purchases: Iterable<Purchase>;
+    const interactor = new PurchasesInteractor(purchases);
+    const model = store(interactor);
+</script>
+
+<div class="purchases report">
+    <div class="stats">
+        <div class="stat">
+            <h5 class="label">{$model.stats.label}</h5>
+            <h3 class="value">{$model.stats.value}</h3>
+        </div>
+    </div>
+
+    <ObservationChart
+        dates={$model.dates}
+        values={$model.values}
+        on:selectDate={event => interactor.selectDate(event.detail)}
+        on:resetDate={interactor.resetDate}>
+
+        <g class="plot" slot="plot" let:x let:y>
+            <Marker x={x} y={y} point={$model.selected}/>
+            <Path x={x} y={y} data={$model.series}/>
+            <Circle x={x} y={y} point={$model.selected}/>
+        </g>
+    </ObservationChart>
+</div>
