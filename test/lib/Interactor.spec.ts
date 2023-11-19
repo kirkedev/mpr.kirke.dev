0527b17d9c41e823111d34737af8c8badb3c8f30
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-19 15:42:57 -0500

    Allow Interactor to accept async Actions

--- test/lib/Interactor.spec.ts
+++ test/lib/Interactor.spec.ts
@@ -8,13 +8,15 @@ const plus = (value: number): Action<number> =>
 const multiply = (value: number): Action<number> =>
     (state: number) => value * state;
 
-test("dispatch actions to interactor to modify state", () => {
+test("dispatch actions to interactor to modify state", async () => {
     const interactor = new Interactor(0);
 
     interactor.execute(plus(1));
+    await tick();
     expect(interactor.state).toBe(1);
 
     interactor.execute(multiply(10));
+    await tick();
     expect(interactor.state).toBe(10);
 });
 
@@ -32,7 +34,6 @@ test("collect interactor states to array", async () => {
     await tick();
 
     close();
-
     expect(interactor.state).toBe(1000);
     expect(states).toEqual([0, 10, 100, 1000]);
 });
@@ -52,10 +53,16 @@ test("multiple subscribers", async () => {
     interactor.execute(multiply(5));
     await tick();
 
+    expect(interactor.subscribers).toBe(3);
     closeFirst();
+
+    expect(interactor.subscribers).toBe(2);
     closeSecond();
+
+    expect(interactor.subscribers).toBe(1);
     closeThird();
 
+    expect(interactor.subscribers).toBe(0);
     expect(first).toEqual([0, 10, 100, 500]);
     expect(second).toEqual(first);
     expect(third).toEqual(second);
