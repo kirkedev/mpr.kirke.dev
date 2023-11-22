57a0eb19621f419af317181d5eafc66dd92dd847
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-22 12:14:13 -0500

    Add collect to async accumulators

--- lib/async/accumulate.ts
+++ lib/async/accumulate.ts
@@ -1,8 +1,13 @@
-import type { Callback } from "..";
+import type { Callback, UnaryOperator } from "..";
 import { iterate } from ".";
+import ObservableState from "./ObservableState";
 
 function each<T>(iterable: AsyncIterable<T>, callback: Callback<T>): Callback<void> {
     const iterator = iterate(iterable);
+
+    if (iterable instanceof ObservableState) {
+        callback(iterable.state);
+    }
 
     (async function() {
         let result = await iterator.next();
@@ -18,4 +23,27 @@ function each<T>(iterable: AsyncIterable<T>, callback: Callback<T>): Callback<vo
     };
 }
 
-export { each };
+function collect<T>(iterable: AsyncIterable<T>): UnaryOperator<void, Array<T>> {
+    const iterator = iterate(iterable);
+    const results = new Array<T>();
+
+    if (iterable instanceof ObservableState) {
+        results.push(iterable.state);
+    }
+
+    (async function() {
+        let result = await iterator.next();
+
+        while (!result.done) {
+            results.push(result.value);
+            result = await iterator.next();
+        }
+    })();
+
+    return (): Array<T> => {
+        iterator.return?.();
+        return results;
+    };
+}
+
+export { each, collect };
