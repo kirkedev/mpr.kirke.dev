24809b17e262a4cbc88fc9c4334189d55e491d6f
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2023-11-29 01:09:11 -0500

    Animate Path component

--- app/ui/Path.svelte
+++ app/ui/Path.svelte
@@ -1,6 +1,11 @@
 <script lang="ts" context="module">
-    import { area } from "d3-shape";
+    import { tweened } from "svelte/motion";
+    import { cubicOut, cubicInOut } from "svelte/easing";
+    import { draw } from "svelte/transition";
     import type { ScaleLinear, ScaleTime } from "d3-scale";
+    import { line } from "d3-shape";
+    import { interpolate } from "polymorph-js";
+    import type { Nullable } from "lib";
     import type Series from "lib/time/Series";
     import type { Data } from "lib/time/Series";
 </script>
@@ -10,9 +15,17 @@
     export let y: ScaleLinear<number, number>;
     export let data: Series;
 
-    $: line = area<Data>()
+    const path = line<Data>()
         .x(data => x(data.date))
         .y(data => y(data.value));
+
+    const d = tweened<Nullable<string>>(null, {
+        duration: 200,
+        easing: cubicOut,
+        interpolate: (a, b) => interpolate([a, b])
+    });
+
+    $: d.set(path(data));
 </script>
 
-<path d={line(data)} class="series"/>
+<path class="series" d={$d} transition:draw={{ duration: 500, easing: cubicInOut }}/>
