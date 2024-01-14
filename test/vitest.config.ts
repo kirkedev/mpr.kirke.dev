2646820c1e1f7fd83682c3cc88bc459c16b0ef2d
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2024-01-14 14:10:49 -0500

    Use v8 instead of istanbul

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -8,7 +8,6 @@ export default mergeConfig(config, {
         include: ["{api,app,lib}/**/*.spec.ts"],
         coverage: {
             all: true,
-            provider: "istanbul",
             include: ["{api,app,lib}/**/*.ts", "app/**/*.svelte"],
             exclude: ["**/*.d.ts", "api/client.ts", "api/index.ts", "app/svg-colors.ts"],
             reportsDirectory: "test/coverage"
