4e3b7cf69d5dac4fa2cec1b28600294aa3aba40d
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2024-02-15 13:43:18 -0500

    Don't fail tests if test api already running

--- test/api/setup.ts
+++ test/api/setup.ts
@@ -1,15 +1,21 @@
 import path from "path";
 import dotenv from "dotenv";
+import detect from "detect-port";
 import type { Callback } from "lib";
 import server from "../mpr/server";
 
 dotenv.config({ path: path.resolve("api", ".env") });
 
 export default async function(): Promise<Callback<void>> {
-    const mpr = await server(3001);
+    try {
+        await detect(3001);
+        const mpr = await server(3001);
 
-    return async function() {
-        mpr.closeAllConnections();
-        await new Promise(resolve => mpr.close(resolve));
-    };
+        return async function() {
+            mpr.closeAllConnections();
+            await new Promise(resolve => mpr.close(resolve));
+        };
+    } catch {
+        return async function() {};
+    }
 }
