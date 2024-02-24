5f139f2607de03b2f8db0fcc1a8bb96e9ce4f633
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2024-02-24 16:36:58 -0500

    Fix pattern matching for path strings

--- test/e2e/primals.spec.ts
+++ test/e2e/primals.spec.ts
@@ -6,31 +6,31 @@ test("View pricing data of each primal", async ({ page }) => {
 
     await test.step("View pricing data for hams", async () => {
         await page.getByText("Ham").click();
-        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", expect.stringMatching(path(540)));
+        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", path(504));
     });
 
     await test.step("View pricing data for pork loins", async () => {
         await page.getByText("Loin").click();
-        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", expect.stringMatching(path(495)));
+        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", path(495));
     });
 
     await test.step("View pricing data for pork butts", async () => {
         await page.getByText("Butt").click();
-        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", expect.stringMatching(path(501)));
+        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", path(501));
     });
 
     await test.step("View pricing data for ribs", async () => {
         await page.getByText("Rib").click();
-        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", expect.stringMatching(path(509)));
+        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", path(509));
     });
 
     await test.step("View pricing data for picnic cuts", async () => {
         await page.getByText("Picnic").click();
-        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", expect.stringMatching(path(496)));
+        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", path(496));
     });
 
     await test.step("View pricing data for pork bellies", async () => {
         await page.getByText("Belly").click();
-        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", expect.stringMatching(path(495)));
+        await expect(page.locator("div[class*=primal] path.series")).toHaveAttribute("d", path(495));
     });
 });
